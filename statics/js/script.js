503ded3a984719957c3548379cf219a2b7d4b4ed
HoLaw77 <>
2023-08-09 12:39:57 +0000

    add dropdown menu

---
+++ statics/js/script.js
@@ -0,0 +1,9 @@
+function showSushiMenu() {
+    document.getElementById("sushidropdown-content").classList.toggle("show");
+}
+
+
+
+function showRamenMenu() {
+    document.getElementById("ramendropdown-content").classList.toggle("show");
+}
