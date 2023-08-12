239040d40605fbeea83ce9b0a8976db5231475db
HoLaw77 <>
2023-08-12 10:49:07 +0000

    export content to index.html

--- order/views.py
+++ order/views.py
@@ -4,4 +4,8 @@ from django.views import generic
 # Create your views here.
 
 
-def food_order(request):
+class SushiList (generic.Listview):
+    model = Sushi
+    queryset = Sushi.object.filter(status=1).order_by('-created_on')
+    template_name="index.html"
+    paginated_by = 3
