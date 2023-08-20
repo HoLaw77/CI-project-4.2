668f9deed5c45f3e1093a8390811e79b1890d9b6
HoLaw77 <>
2023-08-20 07:35:35 +0000

    add form in form.html

--- order/views.py
+++ order/views.py
@@ -29,3 +29,11 @@ def get_form(request):
             form = BookTimeForm()
 
     return render(request, "order.html", {"form": form})
+
+
+def get_task_list(request):
+    order = Order.objects.all()
+    context = {
+        'order': order
+    }
+    return render(request, 'templates/order.html', context)
