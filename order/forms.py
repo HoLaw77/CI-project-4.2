979d7327497c4830d8b5f033b4414f77dec29199
HoLaw77 <>
2023-08-28 14:29:03 +0000

    build form in html

--- order/forms.py
+++ order/forms.py
@@ -3,6 +3,8 @@ import datetime as dt
 
 
 class BookTimeForm(forms.Form):
+    Name = forms.CharField(max_length=100)
     Time = forms.CharField(max_length=100)
     Date = forms.CharField(widget=forms.Textarea)
     People = forms.CharField(widget=forms.Textarea)
+    Email = forms.CharField(max_length=100)
