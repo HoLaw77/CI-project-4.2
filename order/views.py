12f93c88a9f17fe06d1889a9f01992207d45c519
HoLaw77 <>
2023-09-19 11:53:16 +0000

    add delete_drink_order function views

--- order/views.py
+++ order/views.py
@@ -85,7 +85,7 @@ def drink_order(request):
     form = DrinkOrder()
     return render(request, "drink/drink.html", {'form':form})
 
-# def delete_drink_order(request, drink_order_id):
-#         order = get_object_or_404(order, id=drink_order_id)
-#         order.delete()
-#         return redirect(drink_order)
+def delete_drink_order(request, drink_order_id):
+    order = get_object_or_404(order, id=drink_order_id)
+    order.delete()
+    return redirect(drink_order)
