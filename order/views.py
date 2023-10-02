2cb2d44b8c08b3dc7a5599e4aace05db2a8767e6
HoLaw77 <>
2023-10-02 20:32:49 +0000

    add edit order function in views for testing

--- order/views.py
+++ order/views.py
@@ -108,3 +108,21 @@ def delete_drink_order(request, order_id):
     order.drink= None
     order.save()
     return redirect(drink_order)
+
+def edit_ramen_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    order.ramen= None
+    order.save()
+    return redirect(ramen_order)
+
+def edit_sushi_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    order.sushi= None
+    order.save()
+    return redirect(ramen_order)
+
+def edit_drink_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    order.drink= None
+    order.save()
+    return redirect(ramen_order)
