9cec1de437ac29692e1d7d8206b8a376bcc990ef
HoLaw77 <>
2023-10-20 12:47:47 +0000

    rewrite test code for test model

--- order/test_models.py
+++ order/test_models.py
@@ -3,12 +3,7 @@ from .models import Ramen, Sushi, Drink, Order, Confirm
 
 class TestModels(TestCase):
 
-    def create_ramen(self, toppings_choice=1, soup_choice=1, side_dish=1):
-        return Ramen.objects.create(toppings_choice=toppings_choice, soup_choice=soup_choice, side_dish=side_dish)
-    
-    def test_topping_function_return_latest_choice_(self):
-        topping = self.create_ramen()
-        self.assertTrue(isinstance(topping, Ramen))
-        self.assertEqual(topping.topping(), topping.toppings_choice)
+
+
         
         
