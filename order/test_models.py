c503201a89b0a150eb98a32fe35c3f250986d85f
HoLaw77 <>
2023-10-22 19:02:11 +0000

    create test for Ramen model

--- order/test_models.py
+++ order/test_models.py
@@ -1,6 +1,20 @@
 from django.test import TestCase
 from .models import Ramen, Sushi, Drink, Order, Confirm
 
+class TestOrderModels(TestCase):
+    def setUp(self):
+        test = Ramen.objects.create(
+            toppings_choice= 1,
+            soup_choice=1,
+            side_dish=1
+            )
+        
+    def test_model_topping(self):
+        topping = Ramen.objects.last()
+        print(topping)
+        self.assertEqual(topping.toppings_choice, 1)
+
+    
 
 
    
