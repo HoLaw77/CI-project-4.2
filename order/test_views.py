4d50262f926102ea6b1db46045cbf631192d597b
HoLaw77 <>
2023-11-01 14:02:07 +0000

    write two more test for not sushi and not drink in ramen and drink

--- order/test_views.py
+++ order/test_views.py
@@ -60,6 +60,16 @@ class TestViews(TestCase):
         self.assertRedirects(response, reverse("drink_order"))
         order.refresh_from_db()
         self.assertEqual(order.ramen, Ramen.objects.get(toppings_choice=1, side_dish=3, soup_choice=2)) 
+
+    def test_post_valid_form_existing_order_and_drink(self):
+        order = Order.objects.create(customer=self.user)
+        order.drink = Drink.objects.create()
+        order.save()
+        response = self.client.post(reverse('ramen_order'), {"toppings_choice": 2, "side_dish": 4, "soup_choice": 3})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("sushi_order"))
+        order.refresh_from_db()
+        self.assertEqual(order.ramen, Ramen.objects.get(toppings_choice=2, side_dish=4, soup_choice=3)) 
     
     def test_post_valid_form_existing_order_and_sushi_and_drink(self):
         order = Order.objects.create(customer=self.user)
@@ -106,6 +116,16 @@ class TestViews(TestCase):
         order.refresh_from_db()
         self.assertEqual(order.sushi, Sushi.objects.get(nigiri_sushi = 6, inari_sushi = 2, maki_sushi = 3, temaki_sushi = 3, soy_oil = 1, wasabi = 1)) 
     
+    def test_sushi_post_valid_form_existing_order_and_drink(self):
+        order = Order.objects.create(customer=self.user)
+        order.drink = Drink.objects.create()
+        order.save()
+        response = self.client.post(reverse('sushi_order'), {"nigiri_sushi": 5, "inari_sushi": 2, "maki_sushi": 1, "temaki_sushi":2, "soy_oil": 1, "wasabi":1})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("ramen_order"))
+        order.refresh_from_db()
+        self.assertEqual(order.sushi, Sushi.objects.get(nigiri_sushi = 5, inari_sushi= 2, maki_sushi=1, temaki_sushi = 2, soy_oil= 1, wasabi= 1)) 
+    
     def test_post_valid_form_existing_order_and_ramen_and_drink(self):
         order = Order.objects.create(customer=self.user)
         order.ramen = Ramen.objects.create()
@@ -141,7 +161,7 @@ class TestViews(TestCase):
         order = Order.objects.get(customer=self.user)
         self.assertEqual(order.drink, Drink.objects.get(sake = 4, beer = 1, choya = 4, green_tea = 2, water = 2)) 
     
-    def test_post_valid_form_existing_order_and_ramen(self):
+    def test_drink_post_valid_form_existing_order_and_ramen(self):
         order = Order.objects.create(customer=self.user)
         order.ramen = Ramen.objects.create()
         order.save()
@@ -150,8 +170,18 @@ class TestViews(TestCase):
         self.assertRedirects(response, reverse("sushi_order"))
         order.refresh_from_db()
         self.assertEqual(order.drink, Drink.objects.get(sake = 3, beer = 2, choya = 1, green_tea = 2, water = 1)) 
+
+    def test_drink_post_valid_form_existing_order_and_sushi(self):
+        order = Order.objects.create(customer=self.user)
+        order.sushi = Sushi.objects.create()
+        order.save()
+        response = self.client.post(reverse('drink_order'), {"sake": 4, "beer": 1, "choya": 2, "green_tea": 2, "water": 1})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("ramen_order"))
+        order.refresh_from_db()
+        self.assertEqual(order.drink, Drink.objects.get(sake = 4, beer = 1, choya = 2, green_tea = 2, water = 1)) 
     
-    def test_post_valid_form_existing_order_and_ramen_and_sushi(self):
+    def test_drink_post_valid_form_existing_order_and_ramen_and_sushi(self):
         order = Order.objects.create(customer=self.user)
         order.ramen = Ramen.objects.create()
         order.sushi = Sushi.objects.create()
