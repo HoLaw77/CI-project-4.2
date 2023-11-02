62beb7900822902d2bafe58feaefee656c71cf5f
HoLaw77 <>
2023-11-02 13:06:05 +0000

    fix test.py format and delete script.js

--- order/test.py
+++ order/test.py
@@ -1,20 +1,16 @@
 from django.test import TestCase
 
 
-
 class TestDjango(TestCase):
 
     def test_if_it_works(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def test_if_it_works2(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def test_if_it_works3(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def test_if_it_works4(self):
-        self.assertEqual(1,1)
-
-
-        
+        self.assertEqual(1, 1)
