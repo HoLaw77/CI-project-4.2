d95e08985a1adc0c196bc77c07ad0b792b1c4edf
HoLaw77 <>
2023-11-03 23:01:37 +0000

    fix settings.py

--- django_task/settings.py
+++ django_task/settings.py
@@ -17,7 +17,7 @@ if os.path.isfile('env.py'):
      import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'django_task.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.environ.join(BASE_DIR, 'templates'), os.environ.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
